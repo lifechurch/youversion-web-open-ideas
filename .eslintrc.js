b6d3d948cf3296f667471781ff9a4ff04d6e57bf
Michael Martin <martinmsm@gmail.com>
2018-04-07 00:29:15 -0500

    Cleaning up the linting errors now that eslint config is in place

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,7 @@ module.exports = {
 		'plugin:import/errors'
 	],
 	rules: {
-		semi: 0,
+		semi: [ "error", "never" ],
 		'max-len': 0,
 		'object-curly-spacing': [2, 'always'],
 		indent: [2, 'tab', {
@@ -71,6 +71,10 @@ module.exports = {
 		'no-else-return': 0,
 		'no-plusplus': 0,
 		// jsx rules
+    "jsx-a11y/anchor-is-valid": [ "error", {
+      "components": [ "Link" ],
+      "specialLink": [ "to" ]
+    }],
 		'jsx-quotes': 0,
 		'react/jsx-filename-extension': [0, {
 			extensions: ['.js', '.jsx']
--- src/components/videos/videos.js
+++ src/components/videos/videos.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export class Videos extends React.Component {
-    render() {
-        return(
-            <div>
-                <h2>Videos are encapsulated here...</h2>
+const Videos = () => {
+	return (
+		<div>
+			<h2>Videos are encapsulated here...</h2>
 
-                { /* TODO: REMOVE THIS PLACEHOLDER SECTION */ }
-                <hr />
-                <Link to="/videos/1/series">Visit series 1</Link>
-            </div>
-        );
-    }
+			{ /* TODO: REMOVE THIS PLACEHOLDER SECTION */ }
+			<hr />
+			<Link to="/videos/1/series">Visit series 1</Link>
+		</div>
+	)
 }
+
+export default Videos
