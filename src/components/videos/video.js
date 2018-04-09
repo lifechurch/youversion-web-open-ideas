2cd56929c1c29755835aad7fdfc6b755410c4164
WagicalGale <alexgaleiii@gmail.com>
2018-04-09 15:44:56 +0100

    Fixed indentation

--- src/components/videos/video.js
+++ src/components/videos/video.js
@@ -2,49 +2,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import { videoDetailsJson } from '../../api/video-api-mock.js'
-import { VerticalSpace, Padded, Heading1, LinkText, Heading3, Body, Button } from '../../melos'
+import { VerticalSpace, Heading1, LinkText, Heading3, Body, Button } from '../../melos'
 
 const Video = ({ match }) => {
-var video = videoDetailsJson.response.data
+	var video = videoDetailsJson.response.data
 
-return (
-	<div>
-		<Helmet>
-			<title>{video.title} - {video.credits} | Videos | The Bible App | Bible.com</title>
-			<meta name="description" content={video.title + " - " + video.credits + " | Videos"} />
-		</Helmet>
+	return (
+		<div>
+			<Helmet>
+				<title>{video.title} - {video.credits} | Videos | The Bible App | Bible.com</title>
+				<meta name="description" content={video.title + " - " + video.credits + " | Videos"} />
+			</Helmet>
 
-		<VerticalSpace padding={40} space={5} width={'1100px'} margin={'0 auto'}>
-			<Heading1 textAlign="left">{video.title}</Heading1>
-			<LinkText size={30}>{video.credits}</LinkText>
+			<VerticalSpace padding={40} space={5} width={'1100px'} margin={'0 auto'}>
+				<Heading1 textAlign="left">{video.title}</Heading1>
+				<LinkText size={30}>{video.credits}</LinkText>
 
-			{/* Video Player */}
-			<div style={{width: '100%', height: '632px', backgroundColor: 'black'}}></div>
+				{/* Video Player */}
+				<div style={{width: '100%', height: '632px', backgroundColor: 'black'}}></div>
 
-			<VerticalSpace space={0} width={'65%'}>
-				<Heading3 muted>RELATED SCRIPTURE</Heading3>
-				{ video.references.map((ref, i) => {
-					return <LinkText key={i} target='_blank' href={'https://www.bible.com/bible/206/' + ref.usfm}>{ref.human}</LinkText>
-				})}
-				<hr />
-				<Body>{video.description}</Body>
+				<VerticalSpace space={0} width={'65%'}>
+					<Heading3 muted>RELATED SCRIPTURE</Heading3>
+					{ video.references.map((ref, i) => {
+						return <LinkText key={i} target='_blank' href={'https://www.bible.com/bible/206/' + ref.usfm}>{ref.human}</LinkText>
+					})}
+					<hr />
+					<Body>{video.description}</Body>
+				</VerticalSpace>
+
+				<VerticalSpace width={'30%'} height={65}>
+					<Button>All Videos</Button>
+				</VerticalSpace>
 			</VerticalSpace>
 
-			<VerticalSpace width={'30%'} height={65}>
-				<Button>All Videos</Button>
-			</VerticalSpace>
-		</VerticalSpace>
-
-	</div>
-)
+		</div>
+	)
 }
 
 Video.propTypes = {
-match: PropTypes.shape({
-	params: PropTypes.shape({
-		topicId: PropTypes.string
-	})
-}).isRequired
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			topicId: PropTypes.string
+		})
+	}).isRequired
 }
 
 export default Video
