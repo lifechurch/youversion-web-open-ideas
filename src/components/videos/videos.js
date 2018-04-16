7d5a27f9de061c919aab6b902315dabe61b45799
Fortunat Mutunda <fotimutunda@gmail.com>
2018-04-16 23:29:32 +0300

    fixes

--- src/components/videos/videos.js
+++ src/components/videos/videos.js
@@ -1,31 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
-import md5 from 'md5'
-import { get } from 'lodash'
-
+import VerticalSpace from '@youversion/melos/dist/components/layouts/VerticalSpace'
 import TitleVideo from './title-video'
 import { titleVideosPage1, titleVideosPage2 } from '../../api/video-api-mock'
-import { VerticalSpace } from '../../melos'
 
 
-const Videos = () => {
-	const videos1 = get(titleVideosPage1, 'response.data.videos', [])
-	return (
-		<div>
-			<Helmet>
-				<title>Watch videos about the Bible | The Bible App | Bible.com</title>
-				<meta name="description" content="Watch videos about the Bible" />
-			</Helmet>
 
-			<VerticalSpace space={10} padding={40}>
-				{ videos1.map((video) => {
-					return (
-						<TitleVideo thumbnail={ get(video, 'thumbnails.1.url', '')} title={get(video, 'title', '')} key={md5(video.id)} />
-					)
-				})}
-			</VerticalSpace>
-		</div>
-	)
+class Videos extends Component {
+
+	render() {
+
+		const { response: { data: { videos } } } = titleVideosPage1
+
+		return (
+			<div>
+				<Helmet>
+					<title>Watch videos about the Bible | The Bible App | Bible.com</title>
+					<meta name="description" content="Watch videos about the Bible" />
+				</Helmet>
+
+				<VerticalSpace space={10} padding={40}>
+					{ videos.map((video) => <TitleVideo { ...video } key={ video.id } />) }
+				</VerticalSpace>
+			</div>
+		)
+	}
 }
 
 export default Videos
