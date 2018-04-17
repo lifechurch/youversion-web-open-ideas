57f264e520a2e86730e258edcab66cb89dab17ff
Bryan Orozco <bryanorozcoweb@gmail.com>
2018-04-17 08:51:34 -0700

    Downgraded npm to v4

--- src/components/thumbnail-component/thumbnail-component.js
+++ src/components/thumbnail-component/thumbnail-component.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Card from '@youversion/melos/dist/components/containers/Card'
-import LazyImage from '@youversion/melos/dist/components/images/LazyImage'
 import Link from '@youversion/melos/dist/components/links/Link'
+import LazyImage from '@youversion/melos/dist/components/images/LazyImage'
 import '../../Thumbnail.css'
 
 const VideoThumbnail = (props) => {
