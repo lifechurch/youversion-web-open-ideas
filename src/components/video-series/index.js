f40ef08cafbae2520b9e142485e7af261823a0c9
MrChristofferson <chrisvan0848@gmail.com>
2018-05-14 00:10:26 -0500

    Add <VideoThumbnail /> and <SeriesHero />

---
+++ src/components/video-series/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import SeriesHero from '../series-hero/series-hero-page/series-hero-page'
+import VideoThumnail from '../thumbnail-component/thumbnails'
+
+const VideoSeriesMain = () => {
+  return (
+    <React.Fragment>
+      <SeriesHero />
+      <VideoThumnail />
+    </React.Fragment>
+  )
+}
+
+export default VideoSeriesMain
+
