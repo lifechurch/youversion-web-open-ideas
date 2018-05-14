27bd2540b6e3f7b873d678efce46e2a55c92a63d
MrChristofferson <chrisvan0848@gmail.com>
2018-05-14 00:31:03 -0500

    Fix auto-save formatting issues again...

--- src/components/video-series/index.js
+++ src/components/video-series/index.js
@@ -4,12 +4,12 @@ import SeriesHero from '../series-hero/series-hero-page/series-hero-page'
 import VideoThumnail from '../thumbnail-component/thumbnails'
 
 const VideoSeriesMain = () => {
-  return (
-    <React.Fragment>
-      <SeriesHero />
-      <VideoThumnail />
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<SeriesHero />
+			<VideoThumnail />
+		</React.Fragment>
+	)
 }
 
 export default VideoSeriesMain
